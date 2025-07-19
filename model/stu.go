7ee950fe7bd51602503a2996d6384c9daaf42db0
陈冠余 <1150542638@qq.com>
2025-07-19 10:24:35 +0800

    fix: 修复预约面试功能，更新数据库结构

--- model/stu.go
+++ model/stu.go
@@ -2,8 +2,8 @@ package model
 
 type Stu struct {
 	BaseModel
-	OpenID    string     `gorm:"column:openid;type:varchar(50);unique;not null;comment:'微信OpenID'" json:"openid"`
-	NetID     string     `gorm:"column:netid;type:varchar(50);unique;not null;comment:'NetID'" json:"netid"`
+	OpenID    string     `gorm:"column:openid;type:varchar(50);unique;comment:'微信OpenID'" json:"openid"`
+	NetID     string     `gorm:"column:netid;type:varchar(50);unique;comment:'NetID'" json:"netid"`
 	Name      string     `gorm:"column:name;not null;comment:'姓名'" json:"name"`
 	Phone     string     `gorm:"column:phone;not null;comment:'电话号码'" json:"phone"`
 	School    string     `gorm:"column:school;not null;comment:'书院'" json:"school"`
@@ -12,7 +12,7 @@ type Stu struct {
 	ToMaster  string     `gorm:"column:tomaster;not null;comment:'想要掌握的技能'" json:"tomaster"`
 	Depart    Department `gorm:"column:depart;type:enum('tech','video','art','none');default:'none';comment:'主选部门'" json:"depart"`
 	Tag       string     `gorm:"column:tag;not null;comment:'标签'" json:"tag"`
-	Interv    *Interv    `gorm:"foreignKey:NetID;references:NetID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;comment:'面试信息'" json:"interv"`
+	Interv    *Interv    `gorm:"foreignKey:NetID;references:NetID" json:"interv"`
 	Message   int        `gorm:"column:message;not null;default:0;comment:'消息'" json:"message"`
 	QueID     int        `gorm:"column:queid;comment:'问题ID'" json:"queid"`
 	Work      string     `gorm:"column:work;default:'';comment:'作品链接'" json:"work"`
