17c9c82a8d6c5f4025493e381ecbab983d843582
陈冠余 <chenguanyu@tiaozhan.com>
2025-08-06 04:32:45 +0000

    fix: Interv中netid级联更新

--- model/stu.go
+++ model/stu.go
@@ -12,7 +12,7 @@ type Stu struct {
 	ToMaster  string     `gorm:"column:tomaster;not null;comment:'想要掌握的技能'" json:"tomaster"`
 	Depart    Department `gorm:"column:depart;type:enum('tech','video','art','none');default:'none';comment:'主选部门'" json:"depart"`
 	Tag       string     `gorm:"column:tag;not null;comment:'标签'" json:"tag"`
-	Interv    *Interv    `gorm:"foreignKey:NetID;references:NetID" json:"interv"`
+	Interv    *Interv    `gorm:"foreignKey:NetID;references:NetID;constraint:OnUpdate:CASCADE,OnDelete:SET NULL" json:"interv"`
 	Message   int        `gorm:"column:message;not null;default:0;comment:'消息'" json:"message"`
 	QueID     int        `gorm:"column:queid;comment:'问题ID'" json:"queid"`
 	Work      string     `gorm:"column:work;default:'';comment:'作品链接'" json:"work"`
